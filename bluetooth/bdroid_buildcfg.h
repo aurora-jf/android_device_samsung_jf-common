87969bffa89229657325aef5e9340be84800062e
xmajefx <xmajefx@gmail.com>
2017-08-15 16:00:11 +0300

    Revert "jflte: common: bluetooth enable wbs"
    This reverts commit f31c620dd15634963ba7f8f81e6c93dd5a11feaa.

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -22,7 +22,4 @@
 #define BTM_WBS_INCLUDED TRUE
 #define BTA_DISABLE_DELAY 100 /* in milliseconds */
 
-#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
-#define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
-
 #endif
